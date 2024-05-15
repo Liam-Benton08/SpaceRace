458e4b3eb8ed72e242599ca8ffcbfbb56c183232
Liam-Benton08 <liambent383@SDS-L-2BHDF63>
2024-05-15 14:49:13 -0400

    Add project files.

---
+++ SpaceRace/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace SpaceRace
+{
+    partial class spaceRace
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.startLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // startLabel
+            // 
+            this.startLabel.BackColor = System.Drawing.Color.Transparent;
+            this.startLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startLabel.ForeColor = System.Drawing.Color.Red;
+            this.startLabel.Location = new System.Drawing.Point(227, 51);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(381, 62);
+            this.startLabel.TabIndex = 0;
+            this.startLabel.Text = "Click Anywhere to Start";
+            this.startLabel.Click += new System.EventHandler(this.startLabel_Click);
+            // 
+            // spaceRace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.startLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "spaceRace";
+            this.Text = "Space Race";
+            this.Click += new System.EventHandler(this.spaceRace_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.spaceRace_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.spaceRace_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.spaceRace_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label startLabel;
+    }
+}
+
---
+++ SpaceRace/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Liam Benton
+//Space Race, May 15 2024
+//This is a two player racing game
+namespace SpaceRace
+{
+    public partial class spaceRace : Form
+    {
+        Rectangle player1 = new Rectangle(200, 400, 30, 50);
+        Rectangle player2 = new Rectangle(500, 400, 30, 50);
+        
+        int playerSpeed = 5;
+
+        bool upPressed = false;
+        bool downPressed = false;
+        bool wPressed = false;
+        bool sPressed = false;
+
+        int startScreen = 0;
+
+        List<Rectangle> Projectiles = new List<Rectangle>();
+
+        SolidBrush limeBrush = new SolidBrush(Color.LimeGreen);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        public spaceRace()
+        {
+            InitializeComponent();
+        }
+
+        private void spaceRace_Click(object sender, EventArgs e)
+        {
+            startLabel.Text = "";
+            startLabel.Visible = false;
+            gameTimer.Enabled = true;
+            gameTimer.Start();
+            
+            startScreen++;
+
+        }
+
+        private void spaceRace_Paint(object sender, PaintEventArgs e)
+        {
+                if (startScreen == 1)
+                {
+                e.Graphics.FillRectangle(limeBrush, player1);
+                e.Graphics.FillRectangle(limeBrush, player2);
+                }
+
+        }
+
+        private void spaceRace_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    upPressed = false;
+                    break;
+                case Keys.Down:
+                    downPressed = false;
+                    break;
+                case Keys.W:
+                    wPressed = false;
+                    break;
+                case Keys.S:
+                    sPressed = false;
+                    break;
+
+            }
+        }
+
+        private void spaceRace_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    upPressed = true;
+                    break;
+                case Keys.Down:
+                    downPressed = true;
+                    break;
+                case Keys.W:
+                    wPressed = true;
+                    break;
+                case Keys.S:
+                    sPressed = true;
+                    break;
+
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //Move players up and down
+            if (wPressed && player1.Y >= 0)
+            {
+                player1.Y -= playerSpeed;
+            }
+
+            if (sPressed && player1.Y <= this.Height - player1.Height)
+            {
+                player1.Y += playerSpeed;
+            }
+
+            if(upPressed && player2.Y >= 0)
+            {
+                player2.Y -= playerSpeed;
+            }
+
+            if(downPressed && player2.Y <= this.Height - player2.Height)
+            {
+                player2.Y += playerSpeed;
+            }
+
+            //
+
+            Refresh();
+        }
+
+        private void startLabel_Click(object sender, EventArgs e)
+        {
+            startLabel.Text = "";
+            startLabel.Visible = false;
+            gameTimer.Enabled = true;
+            gameTimer.Start();
+
+            startScreen++;
+
+        }
+    }
+}
